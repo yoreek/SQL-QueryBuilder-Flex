2db50cf1e27dd774d1244ebd3d32c6d22e567eb7
Yuriy <yoreek@yahoo.com>
2013-08-15 14:17:20 +0300

    Refactoring

--- lib/SQL/QueryBuilder/Join.pm
+++ lib/SQL/QueryBuilder/Join.pm
@@ -6,76 +6,76 @@ use SQL::QueryBuilder::CondList;
 use base 'SQL::QueryBuilder::Statement';
 
 sub new {
-	my ($class, @options) = @_;
-	my $self = $class->SUPER::new(
-		type    => undef,
-		table   => undef,
-		alias   => undef,
-		on      => undef,
-		using   => [],
-		@options,
-	);
-	return $self;
+    my ($class, @options) = @_;
+    my $self = $class->SUPER::new(
+        type    => undef,
+        table   => undef,
+        alias   => undef,
+        on      => undef,
+        using   => [],
+        @options,
+    );
+    return $self;
 }
 
 sub table {
-	my ($self) = @_;
-	return $self->{table};
+    my ($self) = @_;
+    return $self->{table};
 }
 
 sub alias {
-	my ($self) = @_;
-	return $self->{alias};
+    my ($self) = @_;
+    return $self->{alias};
 }
 
 sub on {
-	my ($self, $condition, @values) = @_;
-	die "The 'USING' clause is already defined"
-		if scalar(@{ $self->{using} });
-	my $cond_list = $self->{on} ||= SQL::QueryBuilder::CondList->new(
-		parent => $self,
-	);
-	return $condition
-		? $cond_list->and($condition, @values)->parent
-		: $cond_list
-	;
+    my ($self, $condition, @values) = @_;
+    die "The 'USING' clause is already defined"
+        if scalar(@{ $self->{using} });
+    my $cond_list = $self->{on} ||= SQL::QueryBuilder::CondList->new(
+        parent => $self,
+    );
+    return $condition
+        ? $cond_list->and($condition, @values)->end()
+        : $cond_list
+    ;
 }
 
 sub using {
-	my ($self, @columns) = @_;
-	die "The 'ON' clause is already defined"
-		if $self->{on};
-	push @{ $self->{using} }, @columns;
-	return $self;
+    my ($self, @columns) = @_;
+    die "The 'ON' clause is already defined"
+        if $self->{on};
+    push @{ $self->{using} }, @columns;
+    return $self;
 }
 
 sub do_build {
-	my ($self, $writer, $indent) = @_;
+    my ($self, $writer, $indent) = @_;
 
-	$indent ||= 0;
+    $indent ||= 0;
 
-	if (ref $self->{table}) {
-		$writer->write(join(' ', $self->{type}, 'JOIN ('), $indent);
-		$self->{table}->build($writer, $indent + 1);
-		$writer->write(join(' AS ',')', $self->{alias}), $indent);
-	}
-	else {
-		my $table = $self->{alias}
-			? join(' ', $self->{table}, $self->{alias})
-			: $self->{table}
-		;
-		$writer->write(join(' ', $self->{type}, 'JOIN', $table), $indent);
-	}
+    if (ref $self->{table}) {
+        $writer->write(join(' ', $self->{type}, 'JOIN ('), $indent);
+        $self->{table}->build($writer, $indent + 1);
+        $writer->write(join(' AS ',')', $self->{alias}), $indent);
+    }
+    else {
+        my $table = $self->{alias}
+            ? join(' ', $self->{table}, $self->{alias})
+            : $self->{table}
+        ;
+        $writer->write(join(' ', $self->{type}, 'JOIN', $table), $indent);
+    }
 
-	if ( $self->{on} ) {
-		$writer->write('ON', $indent);
-		$self->{on}->build($writer, $indent + 1);
-	}
-	elsif ( scalar(@{ $self->{using} }) ) {
-		$writer->write('USING ('. join(', ', @{ $self->{using} }). ')', $indent);
-	}
+    if ( $self->{on} ) {
+        $writer->write('ON', $indent);
+        $self->{on}->build($writer, $indent + 1);
+    }
+    elsif ( scalar(@{ $self->{using} }) ) {
+        $writer->write('USING ('. join(', ', @{ $self->{using} }). ')', $indent);
+    }
 
-	return;
+    return;
 }
 
 1;
--- lib/SQL/QueryBuilder/Statement.pm
+++ lib/SQL/QueryBuilder/Statement.pm
@@ -6,57 +6,57 @@ use SQL::QueryBuilder::Writer;
 use Scalar::Util qw(weaken);
 
 sub new {
-	my ($class, @options) = @_;
+    my ($class, @options) = @_;
 
-	my $self = bless {
-		parent  => undef,
-		_writer => undef,
-		@options,
-	}, $class;
+    my $self = bless {
+        parent  => undef,
+        _writer => undef,
+        @options,
+    }, $class;
 
-	weaken( $self->{parent} );
+    weaken( $self->{parent} );
 
-	return $self;
+    return $self;
 }
 
-sub parent {
-	my ($self) = @_;
-	return $self->{parent};
+sub end {
+    my ($self) = @_;
+    return $self->{parent};
 }
 
 sub get_writer {
-	my ($self) = @_;
-	return $self->{_writer} ||= SQL::QueryBuilder::Writer->new();
+    my ($self) = @_;
+    return $self->{_writer} ||= SQL::QueryBuilder::Writer->new();
 }
 
 sub set_writer {
-	my ($self, $writer) = @_;
-	$self->{_writer} = $writer;
-	return;
+    my ($self, $writer) = @_;
+    $self->{_writer} = $writer;
+    return;
 }
 
 sub build {
-	my ($self, $writer, $indent) = @_;
-	if ($writer) {
-		$self->set_writer($writer);
-	}
-	else {
-		$writer = $self->get_writer();
-		$writer->clear();
-	}
-	$self->do_build($writer, $indent);
-	return;
+    my ($self, $writer, $indent) = @_;
+    if ($writer) {
+        $self->set_writer($writer);
+    }
+    else {
+        $writer = $self->get_writer();
+        $writer->clear();
+    }
+    $self->do_build($writer, $indent);
+    return;
 }
 
 sub do_build {
-	die "Not implemented";
+    die "Not implemented";
 }
 
 sub to_sql {
-	my ($self, $indent) = @_;
-	$self->build();
-	my $writer = $self->get_writer();
-	return $writer->to_sql($indent), $writer->get_params();
+    my ($self, $indent) = @_;
+    $self->build();
+    my $writer = $self->get_writer();
+    return $writer->to_sql($indent), $writer->get_params();
 }
 
 1;
